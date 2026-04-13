c791aea13bb91e2097859ecfb65aa200d015d069
Crispen Gari <crispengari@gmail.com>
2026-04-13 20:43:23 +0200

    concurrecy problem

---
+++ 04_Concurecy/src/Main.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.io.*;
+
+public  class Main {
+    public static boolean prime(int n){
+        if(n<=1) return false;
+        int c = 0;
+        for(int i =1; i<=n; i++){
+            if(n%i == 0) c++;
+            if(c >= 3) break;
+        }
+        return c == 2;
+    }
+    public static void main(String [] args) throws IOException {
+        String basePath = "C://Users//crisp//Desktop//TUT//First Week//Hello there//src//";
+        FileWriter writer = new FileWriter(basePath + "prime.txt");
+        Thread th1 = new Thread(
+                new Producer("producer1.txt", writer)
+        );
+        Thread th2 = new Thread(
+                new Producer("producer2.txt", writer)
+        );
+        Thread th3 = new Thread(
+                new Producer("producer3.txt", writer)
+        );
+        th1.start();
+        th2.start();
+        th3.start();
+
+        try{
+            th1.join();
+            th2.join();
+            th3.join();
+        }catch(InterruptedException e){
+            System.out.println("Something went wrong.");
+        }
+        writer.close();
+    }
+ }
+
+
+ class Producer implements  Runnable {
+    private final String file;
+    private final FileWriter writer;
+    public Producer(String file, FileWriter writer){
+       this.file = file;
+       this.writer = writer;
+    }
+     @Override
+     public void run() {
+         String basePath = "C://Users//crisp//Desktop//TUT//First Week//Hello there//src//";
+         try {
+             File file = new File(basePath + this.file);
+             if(!file.exists()) throw new FileNotFoundException("The file was not found");
+             Scanner reader = new Scanner(file);
+             while(reader.hasNextLine()){
+                 int num = reader.nextInt();
+                 if(Main.prime(num)){
+                     this.writePrimes(num);
+                 }
+             }
+             reader.close();
+         }catch (Exception e){
+             System.out.println(e.getMessage());
+         }
+     }
+     public void writePrimes(int num) throws IOException{
+         synchronized (this) {this.writer.write(num + "\n");}
+     }
+ }
